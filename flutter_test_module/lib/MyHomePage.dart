c6ae2eea66898184efd24de1f0a2fe33503e30e1
wujianhong <wujianhong@wps.cn>
2022-07-22 11:47:09 +0800

    增加titlebarIcon

--- flutter_test_module/lib/MyHomePage.dart
+++ flutter_test_module/lib/MyHomePage.dart
@@ -51,6 +51,35 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: "mennu",
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              },
+            ),
+          ],
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
