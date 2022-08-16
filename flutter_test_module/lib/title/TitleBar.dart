4aac1d5dbdf37622a74eeb0f630c8d05fa98aecf
wujianhong <wujianhong@wps.cn>
2022-08-16 17:12:27 +0800

    titlebar点击换成toast

--- flutter_test_module/lib/title/TitleBar.dart
+++ flutter_test_module/lib/title/TitleBar.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test_module/MyHomePage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class TitleBar extends StatelessWidget implements PreferredSizeWidget{
   TitleBar({
@@ -22,7 +23,7 @@ class TitleBar extends StatelessWidget implements PreferredSizeWidget{
           semanticLabel: "mennu",
         ),
         onPressed: () {
-          print('Menu button');
+          toastShow('mennu');
         },
       ),
       actions: <Widget>[
@@ -32,7 +33,7 @@ class TitleBar extends StatelessWidget implements PreferredSizeWidget{
             semanticLabel: 'search',
           ),
           onPressed: () {
-            print('Search button');
+            toastShow('search');
           },
         ),
         IconButton(
@@ -41,7 +42,7 @@ class TitleBar extends StatelessWidget implements PreferredSizeWidget{
             semanticLabel: 'filter',
           ),
           onPressed: () {
-            print('Filter button');
+            toastShow('filter');
           },
         ),
       ],
@@ -49,6 +50,17 @@ class TitleBar extends StatelessWidget implements PreferredSizeWidget{
       // the App.build method, and use it to set our appbar title.
       title: Text(widget.title),
     );
+  }
+
+  void toastShow(String str) {
+    Fluttertoast.showToast(
+        msg: str,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black45,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 
   @override
