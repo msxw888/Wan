2ac75161ff256d1d1ed1511ebc5959a391f6a226
wujianhong <wujianhong@wps.cn>
2022-08-24 10:20:55 +0800

    解决下被软键盘阻挡的问题

--- flutter_test_module/lib/gesNum/GesNumbers.dart
+++ flutter_test_module/lib/gesNum/GesNumbers.dart
@@ -16,28 +16,57 @@ class _GesNumbersPageState extends State<GesNumbersPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("历史数据"),
-        Container(
-          height: 250,
-          child: ListView(
-            children: [
-          ],),
-        ),
-        Container(height: 1, color: Colors.lightBlue,),
-        Text(num.toString()),
-        ElevatedButton(
-          child: const Text("随机数字"),
-          onPressed: () {
-            setState(() {
-              num = random.nextInt(49) + 1; //左闭右开，+1
-              historyNum.add(num);
-            });
-          },
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("历史数据"),
+          Container(
+            height: 300,
+            child: ListView(
+              children: _getList(),
+            ),
+          ),
+          Container(
+            height: 1,
+            color: Colors.lightBlue,
+          ),
+          Text(num.toString()),
+          ElevatedButton(
+            child: const Text("随机数字"),
+            onPressed: () {
+              setState(() {
+                num = random.nextInt(49) + 1; //左闭右开，+1
+                historyNum.add(num);
+              });
+            },
+          ),
+
+          Container(
+            width: 300,
+            child: TextField(
+
+              autofocus: true,
+              decoration: InputDecoration(
+                  hintText: "用户名或邮箱",
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Widget> _getList() {
+    List<Widget> list = [];
+    if (historyNum == null || list.isNotEmpty) {
+      return list;
+    }
+    for (int value in historyNum.reversed) {
+      list.add(ListTile(
+        title: Text(value.toString()),
+      ));
+    }
+    return list;
   }
 }
