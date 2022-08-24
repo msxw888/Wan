13a5c0e8c96261fafd97267572631a45bf07b5ce
wujianhong <wujianhong@wps.cn>
2022-08-24 11:04:05 +0800

    存一手，数据计算

--- flutter_test_module/lib/gesNum/GesNumbers.dart
+++ flutter_test_module/lib/gesNum/GesNumbers.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,13 @@ class GesNumbersPage extends StatefulWidget {
 class _GesNumbersPageState extends State<GesNumbersPage> {
   Random random = Random();
   int num = 0;
+  int successNum = 0;
   List historyNum = [];
+  List gesNumHistory = [];
+  List gesResult = [];
+
+  //定义一个controller
+  TextEditingController _gesController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +27,20 @@ class _GesNumbersPageState extends State<GesNumbersPage> {
       child: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Text("猜数成功率" + _getResRate()),
           Text("历史数据"),
+          Container(
+            height: 1,
+            color: Colors.lightBlue,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("随机数"),
+              Text("猜数"),
+              Text("结果"),
+            ],
+          ),
           Container(
             height: 300,
             child: ListView(
@@ -38,17 +58,29 @@ class _GesNumbersPageState extends State<GesNumbersPage> {
               setState(() {
                 num = random.nextInt(49) + 1; //左闭右开，+1
                 historyNum.add(num);
+                var gesNumStr = _gesController.text;
+                int gesNum;
+                try {
+                  gesNum = int.parse(gesNumStr);
+                } catch (e) {
+                  gesNum = 1;
+                }
+                gesNumHistory.add(gesNum);
+                bool gesres = gesNum == num;
+                gesResult.add(gesres);
+                if (gesres) {
+                  successNum++;
+                }
               });
             },
           ),
-
           Container(
             width: 300,
             child: TextField(
-
+              controller: _gesController,
               autofocus: true,
               decoration: InputDecoration(
-                  hintText: "用户名或邮箱",
+                hintText: "猜数字，不输入默认为1",
               ),
             ),
           ),
@@ -62,11 +94,35 @@ class _GesNumbersPageState extends State<GesNumbersPage> {
     if (historyNum == null || list.isNotEmpty) {
       return list;
     }
-    for (int value in historyNum.reversed) {
-      list.add(ListTile(
-        title: Text(value.toString()),
-      ));
+    int len = historyNum.length;
+    for (int index = len - 1; index > 0; index--) {
+      list.add(
+        Container(
+          color: _getColor(index),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(historyNum[index].toString()),
+              Text(gesNumHistory[index].toString()),
+              Text(gesResult[index].toString()),
+            ],
+          ),
+        ),
+      );
     }
     return list;
+  }
+
+  String _getResRate() {
+    double rate = (successNum / gesResult.length);
+    return rate.toStringAsFixed(3);
+  }
+
+  Color _getColor(int index) {
+    if(gesResult[index]) {
+      return Colors.red;
+    } else {
+      return Colors.transparent;
+    }
   }
 }
